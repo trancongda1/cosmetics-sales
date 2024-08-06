cf8dfc07c6893bc9acfaa3ba5d42f41a55eb42a3
Trần Công Đạt <dat.tc@digidinos.com>
2024-08-06 16:21:32 +0700

    fix conflic

---
+++ cosmetics-sales/resources/views/client.blade.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Client</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Open+Sans:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         <div class="container-fluid">
+            <nav class="navbar navbar-light bg-light justify-content-between">
+                  <div id="mySidenav" class="sidenav">
+                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                     <a href="/index">Home</a>
+                     <a href="/products">Products</a>
+                     <a href="/about">About</a>
+                     <a href="/client">Client</a>
+                     <a href="/contact">Contact</a>
+                  </div>
+               <span class="toggle_icon" onclick="openNav()"><img src="images/toggle-icon.png"></span>
+               <a class="logo" href="index.html"><img src="images/logo.png"></a></a>
+               <form class="form-inline ">
+                  <div class="login_text">
+                     <ul>
+                        <li><a href="#"><img src="images/user-icon.png"></a></li>
+                        <li><a href="#"><img src="images/bag-icon.png"></a></li>
+                        <li><a href="#"><img src="images/search-icon.png"></a></li>
+                     </ul>
+                  </div>
+               </form>
+            </nav>
+         </div>
+      </div>
+      <!-- header section end -->
+      <!-- customer section start -->
+      <div class="customer_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <h1 class="customer_taital">What says customers</h1>
+               </div>
+            </div>
+            <div id="main_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="client_section_2">
+                        <div class="client_main">
+                           <div class="client_left">
+                              <div class="client_img"><img src="images/vinh.jpeg"></div>
+                           </div>
+                           <div class="client_right">
+                              <h3 class="name_text">Trần Vinh </h3>
+                              <p class="dolor_text">Sản phẩm nhập khẩu từ Hàn Quốc của Như Quỳnh thực sự làm tôi say mê. Không chỉ về chất lượng mà còn về dịch vụ khách hàng. Cảm ơn bạn đã mang lại trải nghiệm mua sắm tuyệt vời! </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="client_section_2">
+                        <div class="client_main">
+                           <div class="client_left">
+                              <div class="client_img"><img src="images/trang.jpeg"></div>
+                           </div>
+                           <div class="client_right">
+                              <h3 class="name_text">Trang Lê</h3>
+                              <p class="dolor_text">Sản phẩm của Như Quỳnh thực sự làm tôi ngạc nhiên! Chất lượng đỉnh cao và đúng như quảng cáo, không gì có thể so sánh được. Rất hài lòng với sự lựa chọn của mình!</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="client_section_2">
+                        <div class="client_main">
+                           <div class="client_left">
+                              <div class="client_img"><img src="images/trinh.jpeg"></div>
+                           </div>
+                           <div class="client_right">
+                              <h3 class="name_text">Trinh Jenny</h3>
+                              <p class="dolor_text">Khi mua mỹ phẩm từ Như Quỳnh, tôi hoàn toàn yên tâm về nguồn gốc và chất lượng. Sản phẩm chính hãng từ Hàn Quốc thực sự đáng khen ngợi. Sẽ luôn là địa chỉ mua sắm hàng đầu của tôi!</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="client_section_2">
+                        <div class="client_main">
+                           <div class="client_left">
+                              <div class="client_img"><img src="images/thu.jpeg"></div>
+                           </div>
+                           <div class="client_right">
+                              <h3 class="name_text">Thu Hana</h3>
+                              <p class="dolor_text">Như Quỳnh thực sự là điểm đến lý tưởng cho những ai đang tìm kiếm sản phẩm chăm sóc da chất lượng từ Hàn Quốc. Sự đa dạng và chất lượng của sản phẩm luôn làm tôi phấn khích và tin tưởng. Cảm ơn Như Quỳnh vì đã mang lại cho tôi một trải nghiệm mua sắm tuyệt vời!</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="client_section_2">
+                        <div class="client_main">
+                           <div class="client_left">
+                              <div class="client_img"><img src="images/dat.jpeg"></div>
+                           </div>
+                           <div class="client_right">
+                              <h3 class="name_text">Dat Chu</h3>
+                              <p class="dolor_text">Sau khi sử dụng sản phẩm của Như Quỳnh, làn da của tôi thực sự cải thiện đáng kể. Sản phẩm không chỉ mang lại hiệu quả ngay từ lần đầu sử dụng mà còn giữ vững tính hiệu quả lâu dài. Tôi không thể hạnh phúc hơn với sự lựa chọn của mình!</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+               <i class="fa fa-angle-left"></i>
+               </a>
+               <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+               <i class="fa fa-angle-right"></i>
+               </a>
+            </div>
+         </div>
+      </div>
+      <!-- customer section end -->
+      <!-- footer section start -->
+      <div class="footer_section layout_padding margin_top90">
+         <div class="container">
+            <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
+            <div class="contact_section_2">
+               <div class="row">
+                  <div class="col-sm-4">
+                     <h3 class="address_text">Contact Us</h3>
+                     <div class="address_bt">
+                        <ul>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left10">Address : 132 Cầu Giấy, Quan Hoa, Cầu Giấy, Hà Nội</span>
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left10">Call : 08677724485</span>
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left10">Email : nhwquynh843@gmail.com</span>
+                              </a>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-sm-4">
+                     <div class="footer_logo_1"><a href="index.html"><img src="images/footer-logo.png"></a></div>
+                     <p class="dummy_text">Dưỡng da hoàn hảo, chăm sóc sắc đẹp toàn diện - Khám phá vẻ đẹp tiềm ẩn và tự tin tỏa sáng mỗi ngày cùng mỹ phẩm cao cấp Như Quỳnh, bí quyết cho làn da rạng</p>
+                  </div>
+                  <div class="col-sm-4">
+                     <div class="main">
+                        <h3 class="address_text">Best Products</h3>
+                        <p class="ipsum_text">NHỮNG SẢN PHẨM TỐT NHẤT
+                           Đem lại cảm giác dễ chịu mà không gây kích ứng. Sản phẩm không chứa bất kỳ thành phần gây hại nào, được phân phối bởi Như Quỳnh</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="social_icon">
+               <ul>
+                   <li>
+                       <a href="https://www.facebook.com/profile.php?id=100026348822058"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                   </li>  
+                   <li>
+                       <a href="https://www.instagram.com/nhwquynhisme/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                   </li>                    
+                   <li>
+                       <a href="tel:0946379685"><i class="fa fa-phone" aria-hidden="true"></i></a>
+                   </li>
+                   <li>
+                       <a href="https://telegram.me/YourTelegramUsername"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+                   </li>
+               </ul>
+           </div>
+           
+         </div>
+      </div>
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container">
+            <p class="copyright_text">Hãy liên hệ với chúng tôi <a href="https://www.instagram.com/nhwquynhisme/">Như Quỳnh</a></p>
+         </div>
+      </div>
+      <!-- copyright section end -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>  
+      <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+      <script>
+         function openNav() {
+           document.getElementById("mySidenav").style.width = "100%";
+         }
+         
+         function closeNav() {
+           document.getElementById("mySidenav").style.width = "0";
+         }
+      </script> 
+   </body>
+</html>
---
+++ cosmetics-sales/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/client', function () {
+    return view('client');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
