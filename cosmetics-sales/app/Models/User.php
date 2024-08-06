563de84d32e774a6e72cc334369e0e15e9bce3f7
Trần Công Đạt <dat.tc@digidinos.com>
2024-08-06 17:15:41 +0700

    123

--- cosmetics-sales/app/Models/User.php
+++ cosmetics-sales/app/Models/User.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +20,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'image_url',
     ];
 
     /**
@@ -29,12 +33,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Get the attributes that should be cast.
      *
-     * @var array<string, string>
+     * @return array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
